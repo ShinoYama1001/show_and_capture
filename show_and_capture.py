df3ce15549ab0485ce8740e4db7d47ac4dd6f0fa
ShinoYama1001 <40195955+ShinoYama1001@users.noreply.github.com>
2018-11-01 16:37:57 +0900

    ディレクトリ毎にデータを保存するように変更

--- show_and_capture.py
+++ show_and_capture.py
@@ -7,10 +7,10 @@ from timeit import default_timer as timer
 
 class Video_Capture(v_c.video_capture):
 
-    def __init__(self, fps):
+    def __init__(self, fps, log_name):
         super().__init__()
         self.out = cv2.VideoWriter('output.mp4', self.fourcc,   fps, (self.re_w,self.re_h), True)
-        self.log = open("log.csv", 'w')
+        self.log = open(log_name, 'w')
 
 
     def draw_to_image(self, frame, image_name):
@@ -58,18 +58,22 @@ class Image_Loader(i_l.image_loader):
 
 
 
-if __name__ == "__main__":
-    VC = Video_Capture(20)
+
+
+def main1():
     IL = Image_Loader("setting.txt")
+    log_name = IL.user_name+"_log.csv"
+    VC = Video_Capture(20, log_name)
+    win_name = "window"
 
     VC.log.write(IL.user_name + '\n')
 
     i=0
     j=0
-    cv2.namedWindow("window", cv2.WINDOW_GUI_NORMAL)
+    cv2.namedWindow(win_name, cv2.WINDOW_GUI_NORMAL)
     #cv2.moveWindow("window", 200,200)
 
-    cv2.imshow("window", IL.image_list[i][j])
+    cv2.imshow(win_name, IL.image_list[i][j])
 
     while True:
         ret, frame = VC.cap.read()
@@ -91,16 +95,75 @@ if __name__ == "__main__":
             if j<0:
                 i = (i-1) % len(IL.image_list)
                 j = len(IL.image_list[i]) - 1
-            cv2.imshow("window", IL.image_list[i][j])
+            cv2.imshow(win_name, IL.image_list[i][j])
 
         if key == "left":
             j += 1
             if j>=len(IL.image_list[i]):
                 j = 0
-                i = (i-1) % len(IL.image_list)
-            cv2.imshow("window", IL.image_list[i][j])
+                i = (i+1) % len(IL.image_list)
+            cv2.imshow(win_name, IL.image_list[i][j])
 
 
         VC.frame_count += 1
 
     VC.capture_end()
+
+def main2():
+    fps = 20
+    IL = Image_Loader("setting.txt")
+    log_name = IL.user_name+"_log.csv"
+    VC = Video_Capture(fps, log_name)
+    win_name = "window"
+
+    VC.log.write(IL.user_name + '\n')
+
+    i,j = 0,0
+    #*動画をディレクトリ毎に分けたいがための上書き
+    VC.out = cv2.VideoWriter(IL.user_name+'_'+IL.dir_names[i]+".mp4", VC.fourcc, fps, (VC.re_w,VC.re_h), True)
+    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(win_name, IL.image_list[i][j])
+
+    while True:
+        ret, frame = VC.cap.read()
+        if ret == False:
+            print("Finish at cap.read")
+            break
+
+        VC.calc_fps()
+        VC.draw_to_image(frame, IL.image_name_list[i][j])
+        VC.output(frame, IL.image_name_list[i][j])
+
+        key = VC.check_keyboard()
+
+        if key == "break":
+            break
+        elif key == "right":
+            j -= 1
+            #*一旦ディレクトリ戻るのはなしに
+            if j<0:
+                j=0
+            cv2.imshow(win_name, IL.image_list[i][j])
+        elif key == "left":
+            j += 1
+            #*ディレクトリの遷移
+            if j >= len(IL.image_list[i]):
+                j = 0
+                i = (i+1) % len(IL.image_list)
+                #*ファイル分け用の処理
+                VC.out.release()
+                VC.out = cv2.VideoWriter(IL.user_name+'_'+IL.dir_names[i]+".mp4", VC.fourcc, fps, (VC.re_w,VC.re_h), True)
+                VC.log.close()
+                VC.log = open(log_name, 'a')
+
+            cv2.imshow(win_name, IL.image_list[i][j])
+
+        VC.frame_count += 1
+    VC.capture_end()
+
+
+
+
+
+if __name__ == "__main__":
+    main2()
