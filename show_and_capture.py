d726d8cf0d733c86728e9e0c0b81dc6b0e9ae4d4
ShinoYama1001 <40195955+ShinoYama1001@users.noreply.github.com>
2018-11-07 16:02:53 +0900

    画像の大きさ調整、上下拡大機能追加　多分完成？

--- show_and_capture.py
+++ show_and_capture.py
@@ -54,6 +54,7 @@ class Video_Capture(v_c.video_capture):
     def capture_end(self):
         super().capture_end()
         self.log.close()
+        os.remove("output.mp4")
 
 class Image_Loader(i_l.image_loader):
     def __init__(self, text_path):
@@ -74,48 +75,63 @@ class Image_Loader(i_l.image_loader):
 class Show_And_Capture():
 
     def __init__(self):
-        #todo ウィンドウフルサイズは下のが邪魔なのでサイズ調整する
+        # ウィンドウの解像度
         self.dw = 1440
-        self.dh = 900
+        self.dh = 900 - 50 #上の枠ぶんはみ出ることに気づいた
         self.fps = 20
         self.IL = Image_Loader("setting.txt")
+        # データ格納用フォルダがなければ作る
         try:
             os.mkdir(self.IL.user_name)
         except FileExistsError:
             pass
+        # 諸々の下準備
         self.log_name = self.IL.user_name+'/'+self.IL.user_name+"_log.csv"
         self.VC = Video_Capture(self.fps, self.log_name)
-        self.win_name = "window"
+        self.win_name = "test_program"
         self.i,self.j = 0,0
-
         self.VC.out = cv2.VideoWriter(self.IL.user_name+'/'+self.IL.user_name+'_'+self.IL.dir_names[self.i]+".mp4", self.VC.fourcc, self.fps, (self.VC.re_w, self.VC.re_h), True)
+        # 最初に表示する分
         cv2.namedWindow(self.win_name, cv2.WINDOW_FULLSCREEN)
         self.show_fitted_image(self.IL.image_list[self.i][self.j])
 
+    # 画像を画面の大きさに合わせて表示する関数　表示には全部これを使う
     def show_fitted_image(self, image):
+        # 画像サイズ取得
         iw = image.shape[1]
         ih = image.shape[0]
-
+        # 画像の比率計算
         as_w = self.dw / iw
         as_h = self.dh / ih
+        # 調整後のサイズ格納用変数
+        resized_w, resized_h = 0,0
 
+        # 縦か横かどっちかに合わせる
         if as_w < as_h:
-            resized_image = cv2.resize(image, (int(iw*as_w), int(ih*as_w)))
+            resized_w = int(iw*as_w)
+            resized_h = int(ih*as_w)
+            resized_image = cv2.resize(image, (resized_w, resized_h))
         else:
-            resized_image = cv2.resize(image, (int(iw*as_h), int(ih*as_h)))
+            resized_w = int(iw*as_h)
+            resized_h = int(ih*as_h)
+            resized_image = cv2.resize(image, (resized_w, resized_h))
+        # 合わせた大きさで表示し、中央へ移動する
         cv2.imshow(self.win_name, resized_image)
-        #todo ここにウィンドウの位置の調整を入れる
+        cv2.moveWindow(self.win_name, int((self.dw - resized_w)/2), int((self.dh - resized_h)/2))
 
+    # キー入力による処理の分岐
     def key_control(self, key):
         if key == "break":
             return "break"
 
+        # 右で戻る　前のディレクトリには戻れない
         elif key == "right":
             self.j -= 1
             if self.j<0:
                 self.j=0
             self.show_fitted_image(self.IL.image_list[self.i][self.j])
 
+        # 左で進む　ディレクトリをまたぐ時、記録ファイルを区切る
         elif key == "left":
             self.j += 1
             if self.j >= len(self.IL.image_list[self.i]):
@@ -130,27 +146,49 @@ class Show_And_Capture():
                     return "break"
             self.show_fitted_image(self.IL.image_list[self.i][self.j])
 
+        # 上で上側を拡大表示
+        elif key == "up":
+            iw = self.IL.image_list[self.i][self.j].shape[1]
+            ih = self.IL.image_list[self.i][self.j].shape[0]
+            cutted_image = self.IL.image_list[self.i][self.j].copy()[0:int(ih*0.6), 0:iw]
+            self.show_fitted_image(cutted_image)
 
+        # 下で下側を拡大表示
+        elif key == "down":
+            iw = self.IL.image_list[self.i][self.j].shape[1]
+            ih = self.IL.image_list[self.i][self.j].shape[0]
+            cutted_image = self.IL.image_list[self.i][self.j].copy()[int(ih*0.4):ih, 0:iw]
+            self.show_fitted_image(cutted_image)
+
+        # スペースで元の大きさに戻す
+        elif key == "space":
+            self.show_fitted_image(self.IL.image_list[self.i][self.j])
+
+    # メインの処理
     def main_loop(self):
         while True:
+            # フレームの取得
             ret, frame = self.VC.cap.read()
             if ret == False:
                 print("False in cap.read")
                 break
 
+            # 諸々出力
             self.VC.calc_fps()
             self.VC.draw_to_image(frame, self.IL.image_name_list[self.i][self.j])
             self.VC.output(frame, self.IL.image_name_list[self.i][self.j])
 
+            # キーボード操作
             key = self.VC.check_keyboard()
             if key != "none":
                 if self.key_control(key) == "break":
                     break
 
             self.VC.frame_count += 1
+        # 出て来たら終わり
         self.VC.capture_end()
 
-
+#! unused method
 def main_ver1():
     IL = Image_Loader("setting.txt")
     log_name = IL.user_name+"_log.csv"
@@ -199,7 +237,7 @@ def main_ver1():
         VC.frame_count += 1
 
     VC.capture_end()
-
+#! unused method
 def main_ver2():
     #ディスプレイの解像度
     dw = 1440
@@ -270,7 +308,7 @@ def main_ver2():
 
 
 
-
+# メイン関数
 if __name__ == "__main__":
     SAC = Show_And_Capture()
     SAC.main_loop()
--- test.py
+++
@@ -1,8 +0,0 @@
-with open("setting.txt") as setting:
-        user_name = setting.readline().strip()
-        dir_name = setting.readline().strip()
-        dir_info = [i.split() for i in setting.readlines()]
-
-        print(user_name)
-        print(dir_name)
-        print(dir_info)
